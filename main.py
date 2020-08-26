2a8d27c14994b3fea584cd986eadc84cbcd90be6
Basketball4Life <69859256+Basketball4Life@users.noreply.github.com>
2020-08-26 11:00:55 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 # Create the game 
 info.set_score(0)
 info.set_life(3)
-# Make the player 
 
+# Make the player 
+mario = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . 2 2 2 2 2 2 2 . . . . . .
+    . . 2 2 2 2 2 2 2 2 2 2 . . . .
+    . . e e e d d f d . . . . . . .
+    . e d e d d d f d d . . . . . .
+    . e d e e d d d f d d . . . . .
+    . e e d d d d f f f f . . . . .
+    . . . d d d d d d d . . . . . .
+    . . 2 2 a 2 2 a 2 . . . . . . .
+    . 2 2 2 a 2 2 a 2 2 . . . . . .
+    2 2 2 2 a a a a 2 2 2 . d . . .
+    2 2 2 a 5 a a 5 a 2 d d d . . .
+    d d 2 a a a a a a 2 d d d . . .
+    d d d a a a a a a 2 2 2 . . . .
+    d d . e e . . e e . . . . . . .
+    . . e e e . . e e e . . . . . .
+"""))
+mario.set_position(10, 60)
+mario.set_flag(SpriteFlag.StayInScreen, True)
 # Make player controls
+controller.move_sprite(mario, 200, 200)
 
 # Create the basket to make the points
 
--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
 //  Create the game 
 info.setScore(0)
 info.setLife(3)
+//  Make the player 
+let mario = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . 2 2 2 2 2 2 2 . . . . . .
+    . . 2 2 2 2 2 2 2 2 2 2 . . . .
+    . . e e e d d f d . . . . . . .
+    . e d e d d d f d d . . . . . .
+    . e d e e d d d f d d . . . . .
+    . e e d d d d f f f f . . . . .
+    . . . d d d d d d d . . . . . .
+    . . 2 2 a 2 2 a 2 . . . . . . .
+    . 2 2 2 a 2 2 a 2 2 . . . . . .
+    2 2 2 2 a a a a 2 2 2 . d . . .
+    2 2 2 a 5 a a 5 a 2 d d d . . .
+    d d 2 a a a a a a 2 d d d . . .
+    d d d a a a a a a 2 2 2 . . . .
+    d d . e e . . e e . . . . . . .
+    . . e e e . . e e e . . . . . .
+`)
+mario.setPosition(10, 60)
+mario.setFlag(SpriteFlag.StayInScreen, true)
+//  Make player controls
+controller.moveSprite(mario, 200, 200)
