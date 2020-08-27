3511f03fb701fd81fcffb32da4027e10a0823195
Basketball4Life <69859256+Basketball4Life@users.noreply.github.com>
2020-08-27 11:16:35 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
-# Create the game 
+# Create the game
 info.set_score(0)
 info.set_life(3)
-
-# Make the player 
+# Make the player
 mario = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . 2 2 2 2 2 2 2 . . . . . .
-    . . 2 2 2 2 2 2 2 2 2 2 . . . .
-    . . e e e d d f d . . . . . . .
-    . e d e d d d f d d . . . . . .
-    . e d e e d d d f d d . . . . .
-    . e e d d d d f f f f . . . . .
-    . . . d d d d d d d . . . . . .
-    . . 2 2 a 2 2 a 2 . . . . . . .
-    . 2 2 2 a 2 2 a 2 2 . . . . . .
-    2 2 2 2 a a a a 2 2 2 . d . . .
-    2 2 2 a 5 a a 5 a 2 d d d . . .
-    d d 2 a a a a a a 2 d d d . . .
-    d d d a a a a a a 2 2 2 . . . .
-    d d . e e . . e e . . . . . . .
-    . . e e e . . e e e . . . . . .
-"""))
+        . . . . . . . . . . . . . . . . 
+            . . . 2 2 2 2 2 2 2 . . . . . . 
+            . . 2 2 2 2 2 2 2 2 2 2 . . . . 
+            . . e e e d d f d . . . . . . . 
+            . e d e d d d f d d . . . . . . 
+            . e d e e d d d f d d . . . . . 
+            . e e d d d d f f f f . . . . . 
+            . . . d d d d d d d . . . . . . 
+            . . 2 2 a 2 2 a 2 . . . . . . . 
+            . 2 2 2 a 2 2 a 2 2 . . . . . . 
+            2 2 2 2 a a a a 2 2 2 . d . . . 
+            2 2 2 a 5 a a 5 a 2 d d d . . . 
+            d d 2 a a a a a a 2 d d d . . . 
+            d d d a a a a a a 2 2 2 . . . . 
+            d d . e e . . e e . . . . . . . 
+            . . e e e . . e e e . . . . . .
+    """),
+    0)
 mario.set_position(10, 60)
-mario.set_flag(SpriteFlag.StayInScreen, True)
+mario.set_flag(SpriteFlag.STAY_IN_SCREEN, True)
 # Make player controls
 controller.move_sprite(mario, 200, 200)
+# Create the enemies
 
-# Create the basket to make the points
-
-# Shoot the projectiles at the basket
+def on_update_interval():
+    goombas = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . e e e e e e . . . .
+    . . . . . e f e e e e f e . . .
+    . . . . e e 1 f e e f 1 e e . .
+    . . . e e e 1 f f f f 1 e e e .
+    . . . e e e 1 f e e f 1 e e e .
+    . . . e e e 1 1 e e 1 1 e e e .
+    . . . . e e e e e e e e e e . .
+    . . . . . d d d d d d d d . . .
+    . . . . . d d d d d d d d . . .
+    . . . f f d d d d d d d d f f .
+    . . . f f f d d d d d d f f f .
+    . . . f f f f . . . . f f f f .
+    . . . . f f f . . . . f f f . .
+    """))
+    goombas.set_position(scene.screen_width(), randint(0,scene.screen_height()))
+    goombas.set_velocity(-30,0)
+    
+game.on_update_interval(900, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,26 +1,49 @@
-//  Create the game 
+//  Create the game
 info.setScore(0)
 info.setLife(3)
-//  Make the player 
+//  Make the player
 let mario = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . 2 2 2 2 2 2 2 . . . . . .
-    . . 2 2 2 2 2 2 2 2 2 2 . . . .
-    . . e e e d d f d . . . . . . .
-    . e d e d d d f d d . . . . . .
-    . e d e e d d d f d d . . . . .
-    . e e d d d d f f f f . . . . .
-    . . . d d d d d d d . . . . . .
-    . . 2 2 a 2 2 a 2 . . . . . . .
-    . 2 2 2 a 2 2 a 2 2 . . . . . .
-    2 2 2 2 a a a a 2 2 2 . d . . .
-    2 2 2 a 5 a a 5 a 2 d d d . . .
-    d d 2 a a a a a a 2 d d d . . .
-    d d d a a a a a a 2 2 2 . . . .
-    d d . e e . . e e . . . . . . .
-    . . e e e . . e e e . . . . . .
-`)
+        . . . . . . . . . . . . . . . . 
+            . . . 2 2 2 2 2 2 2 . . . . . . 
+            . . 2 2 2 2 2 2 2 2 2 2 . . . . 
+            . . e e e d d f d . . . . . . . 
+            . e d e d d d f d d . . . . . . 
+            . e d e e d d d f d d . . . . . 
+            . e e d d d d f f f f . . . . . 
+            . . . d d d d d d d . . . . . . 
+            . . 2 2 a 2 2 a 2 . . . . . . . 
+            . 2 2 2 a 2 2 a 2 2 . . . . . . 
+            2 2 2 2 a a a a 2 2 2 . d . . . 
+            2 2 2 a 5 a a 5 a 2 d d d . . . 
+            d d 2 a a a a a a 2 d d d . . . 
+            d d d a a a a a a 2 2 2 . . . . 
+            d d . e e . . e e . . . . . . . 
+            . . e e e . . e e e . . . . . .
+    `, 0)
 mario.setPosition(10, 60)
 mario.setFlag(SpriteFlag.StayInScreen, true)
 //  Make player controls
 controller.moveSprite(mario, 200, 200)
+//  Create the enemies
+game.onUpdateInterval(900, function on_update_interval() {
+    let goombas = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . e e e e e e . . . .
+    . . . . . e f e e e e f e . . .
+    . . . . e e 1 f e e f 1 e e . .
+    . . . e e e 1 f f f f 1 e e e .
+    . . . e e e 1 f e e f 1 e e e .
+    . . . e e e 1 1 e e 1 1 e e e .
+    . . . . e e e e e e e e e e . .
+    . . . . . d d d d d d d d . . .
+    . . . . . d d d d d d d d . . .
+    . . . f f d d d d d d d d f f .
+    . . . f f f d d d d d d f f f .
+    . . . f f f f . . . . f f f f .
+    . . . . f f f . . . . f f f . .
+    `)
+    goombas.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
+    goombas.setVelocity(-30, 0)
+})
